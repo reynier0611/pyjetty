ef39032974dc468de706ce7f5a9a2a6c8fb94a22
reynier <reynier0611@gmail.com>
2020-10-19 11:32:46 -0700

    Added code to merge rescaled MC pthat files

---
+++ pyjetty/alice_analysis/slurm/utils/rey/merge_pthat.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#
+# Script to merge output ROOT files from all pt-hat bins together, in stages
+JOB_ID=209384
+FILE_DIR=/rstorage/alice/AnalysisResults/rey/$JOB_ID
+OUTPUT_DIR=/rstorage/alice/AnalysisResults/rey/$JOB_ID
+
+# Merge all output files from each pt-hat bin
+hadd -f -j 10 $OUTPUT_DIR/AnalysisResultsFinal.root $FILE_DIR/Stage0/*/*.root
