28b0157c16e43253dae3d986d64e92ae6c917ab8
reynier <reynier0611@gmail.com>
2020-11-19 11:30:16 -0800

    merge different pthat bins, but locally rather than submitting it as a job

---
+++ pyjetty/alice_analysis/slurm/utils/rey/local_merge_LHC18b8.sh
@@ -0,0 +1,28 @@
+JOB_ID=262562
+OUTPUT_DIR="/rstorage/alice/AnalysisResults/rey/$JOB_ID"
+
+# command line arguments
+if [ "$1" != "" ]; then
+  MERGE_JOB_ID=$1
+  echo "Merge Job ID: $MERGE_JOB_ID"
+else
+  echo "Wrong command line arguments"
+fi
+
+if [ "$2" != "" ]; then
+  BIN=$2
+  echo "Bin: $BIN"
+else
+  echo "Wrong command line arguments"
+fi
+
+# Merge all output files from each pt-hat bin
+FILE_DIR_BASE=/rstorage/alice/AnalysisResults/rey/$JOB_ID
+FILES=$( find ${FILE_DIR_BASE}/LHC18b8/520/child_*/TrainOutput/${BIN}/*/*/ -name "*.root" )
+
+OUT_DIR_BASE=/rstorage/alice/AnalysisResults/rey/$JOB_ID
+mkdir -p ${OUT_DIR_BASE}/Stage0/${BIN}
+hadd -f -j 10 ${OUT_DIR_BASE}/Stage0/${BIN}/AnalysisResults.root $FILES
+
+# Move stdout to appropriate folder
+mv /rstorage/alice/AnalysisResults/rey/slurm-${MERGE_JOB_ID}_${BIN}.out /rstorage/alice/AnalysisResults/rey/${JOB_ID}/
---
+++ pyjetty/alice_analysis/slurm/utils/rey/sourceme_local_merge_LHC18b8.sh
@@ -0,0 +1,4 @@
+for pthat in {1..20}
+do
+	source local_merge_LHC18b8.sh 1 $pthat
+done
