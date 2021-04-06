810795b3c55fe88215acefbe98be393662910ba3
reynier <reynier0611@gmail.com>
2021-04-06 07:15:51 -0700

    Made shell script to run analysis a bit more general

--- pyjetty/alice_analysis/sourceme.sh
+++ pyjetty/alice_analysis/sourceme.sh
@@ -2,6 +2,8 @@ OUTDIR="round_71"
 
 CONFIGDIR="/home/rey/pyjetty/pyjetty/alice_analysis/config/jet_axis/configs_broken_down_in_pT_and_jetR/"
 CONFIGCOPY=$CONFIGDIR$OUTDIR
+OUTPATH="/rstorage/alice/AnalysisResults/rey/"
+CODEPATH="~/pyjetty/pyjetty/alice_analysis/"
 
 # Check if there's a directory called $OUTDIR in the main config directory. If it does not exist, create one.
 if [[ ! -e $CONFIGCOPY ]]; then
@@ -12,10 +14,10 @@ fi
 for R in 0.4 #0.2
 do
 	# loop over the pT bins being considered
-	for pT in "20_40" "40_60" "60_80" "80_100" #"20_100"
+	for pT in "20_40" "40_60" "60_80" "80_100" "20_100"
 	do
 		# loop over types of jet axes
-		for ax in "Standard_SD" #"WTA"
+		for ax in "Standard_SD" "WTA"
 		do
                         # Run the analysis
 			CONFIG=$CONFIGDIR"rey_pp_R_"$R"_pT_"$pT"_GeV_"$ax".yaml"
@@ -25,7 +27,7 @@ do
 			python analysis/user/rey/run_analysis_jet_axis.py -c $CONFIG 
 
                         # Go to the output file and organize the directories
-			goout
+			cd $OUTPATH
 			cd jet_axis/
 
                         # Create the output directories	
@@ -41,13 +43,13 @@ do
 				mv $folder $name
 			done
 
-			goana
+			cd $CODEPATH
 		done
 	done
 done
 
-goout
+cd $OUTPATH
 cd jet_axis/
 mkdir $OUTDIR
 mv new_* $OUTDIR
-goana
+cd $CODEPATH
